fc001be05ab20438559a803cc9371d183e74ee57
chengyutong <chengyt2001@gmail.com>
2025-10-13 00:42:05 +0800

    Change versionCode to 3 for Google Play upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // 明確設定最低版本為21
         targetSdk = 36
-        versionCode = 2 //flutter.versionCode
+        versionCode = 3 //flutter.versionCode
         versionName = flutter.versionName
     }
 
