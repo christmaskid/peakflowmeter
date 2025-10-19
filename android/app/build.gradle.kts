6627029bc058f6aedf886868495a098f5ea92b46
chengyutong <chengyt2001@gmail.com>
2025-10-20 00:02:12 +0800

    Update version number to 1.0.4 for publish

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 4 //flutter.versionCode
+        versionCode = 5 //flutter.versionCode
         versionName = flutter.versionName
     }
 
